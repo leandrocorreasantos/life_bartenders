f157e20181867d93b81f353be3e78e148e738947
leandro <leandro.admo@gmail.com>
2020-03-18 19:32:38 -0300

    pastas static e media no .env

--- lifebartenders/settings.py
+++ lifebartenders/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = env('STATIC_ROOT')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = env('MEDIA_ROOT')
 MEDIA_URL = '/media/'
